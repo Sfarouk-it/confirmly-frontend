579d74cfd44cc149bacb649bc120f48341c860b6
farouk <farouk2003s@gmail.com>
2025-11-01 16:51:22 +0100

    new one

--- src/pages/HomePage.tsx/components/HomeNavBar.tsx
+++ src/pages/HomePage.tsx/components/HomeNavBar.tsx
@@ -23,11 +23,11 @@ const HomeNavBar = () => {
             <div className={nav ? "fixed left-0 top-0 w-[60%] border-r border-r-blue-600 h-full bg-white ease-in-out duration-500" : "fixed left-[-100%]"}>
                 <h1 className="w-full text-3xl font-bold text-blue-600 p-4">confirmly</h1>
                 <ul className="pt-12 uppercase">
-                    <li className="p-4 border-b border-b-blue-600">Home</li>
-                    <li className="p-4 border-b border-b-blue-600">Company</li>
-                    <li className="p-4 border-b border-b-blue-600">Services</li>
-                    <li className="p-4 border-b border-b-blue-600">Price</li>
-                    <li className="p-4 border-b border-b-blue-600">Contact</li>
+                    <li className="p-4 border-b border-b-blue-600"><a href="#home">Home</a></li>
+                    <li className="p-4 border-b border-b-blue-600"><a href="#company">Company</a></li>
+                    <li className="p-4 border-b border-b-blue-600"><a href="#services">Services</a></li>
+                    <li className="p-4 border-b border-b-blue-600"><a href="#price">Price</a></li>
+                    <li className="p-4 border-b border-b-blue-600"><a href="#contact">Contact</a></li>
                 </ul>
             </div>
         </div>
